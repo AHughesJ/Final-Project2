793f6c3fc7af9a3a0fdaa266e3888c611545bdbc
ANDREW-PC\Andrew <2andrew.hughes@gmail.com>
2017-04-05 19:35:33 -0400

    NPV Logic additions

--- Final Project/NPV Calculator.vb
+++ Final Project/NPV Calculator.vb
@@ -32,6 +32,7 @@ Public Class NPV_Calculator
     Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
         Dim EnteredCF As Double 'The Cashflow entered by the user 
         Dim CfAccumulator As Double = 0 'Accumulates the Cashflows after they are calculated
+        Dim expCounter As Integer = 1   'Exponent counter, each period the denominator is raised by +1
 
         amount = CInt(txtCfQuantity.Text)           'assigns the user input to amount (amount of cashflow periods)
         Dim Values(amount - 1) As Double            'amount variable input defines the array length, - 1 since vba counts 0
@@ -40,7 +41,9 @@ Public Class NPV_Calculator
 
             EnteredCF = CDbl(InputBox("Enter The Cashflow for this period", "Cash Flow", "0"))
 
-            CfAccumulator = CfAccumulator + CDbl(EnteredCF / (1 + (0.1) ^ Values.Length)) 'need to add array loop here to calculate denominator  
+            CfAccumulator = CfAccumulator + CDbl(EnteredCF / (1 + 0.1) ^ expCounter) 'need to add array loop here to calculate denominator 
+
+            expCounter = expCounter + 1 'Increase exponent by 1 for each period
         Next
         MessageBox.Show(CfAccumulator.ToString)         'For test only 
     End Sub
