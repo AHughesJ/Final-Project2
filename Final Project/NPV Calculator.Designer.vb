ad1abcc4087df11db26c427fb9f4a0cf8223fb64
ANDREW-PC\Andrew <2andrew.hughes@gmail.com>
2017-04-05 20:39:54 -0400

    test

--- Final Project/NPV Calculator.Designer.vb
+++ Final Project/NPV Calculator.Designer.vb
@@ -24,33 +24,106 @@ Partial Class NPV_Calculator
     Private Sub InitializeComponent()
         Me.txtCfQuantity = New System.Windows.Forms.TextBox()
         Me.btnCalc = New System.Windows.Forms.Button()
+        Me.lblCF = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtDiscountRate = New System.Windows.Forms.TextBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ProjectListToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.lblStatus = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.MenuStrip1.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'txtCfQuantity
         '
-        Me.txtCfQuantity.Location = New System.Drawing.Point(86, 102)
+        Me.txtCfQuantity.Location = New System.Drawing.Point(124, 129)
         Me.txtCfQuantity.Name = "txtCfQuantity"
         Me.txtCfQuantity.Size = New System.Drawing.Size(100, 20)
         Me.txtCfQuantity.TabIndex = 0
         '
         'btnCalc
         '
-        Me.btnCalc.Location = New System.Drawing.Point(118, 186)
+        Me.btnCalc.Location = New System.Drawing.Point(124, 168)
         Me.btnCalc.Name = "btnCalc"
-        Me.btnCalc.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalc.Size = New System.Drawing.Size(100, 36)
         Me.btnCalc.TabIndex = 1
-        Me.btnCalc.Text = "btnCalc"
+        Me.btnCalc.Text = "Calculate"
         Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'lblCF
+        '
+        Me.lblCF.AutoSize = True
+        Me.lblCF.Location = New System.Drawing.Point(12, 132)
+        Me.lblCF.Name = "lblCF"
+        Me.lblCF.Size = New System.Drawing.Size(90, 13)
+        Me.lblCF.TabIndex = 2
+        Me.lblCF.Text = "Insert CF Periods:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 97)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(107, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Insert Discount Rate:"
+        '
+        'txtDiscountRate
+        '
+        Me.txtDiscountRate.Location = New System.Drawing.Point(125, 94)
+        Me.txtDiscountRate.Name = "txtDiscountRate"
+        Me.txtDiscountRate.Size = New System.Drawing.Size(100, 20)
+        Me.txtDiscountRate.TabIndex = 3
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ProjectListToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(315, 24)
+        Me.MenuStrip1.TabIndex = 4
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ProjectListToolStripMenuItem
+        '
+        Me.ProjectListToolStripMenuItem.Name = "ProjectListToolStripMenuItem"
+        Me.ProjectListToolStripMenuItem.Size = New System.Drawing.Size(77, 20)
+        Me.ProjectListToolStripMenuItem.Text = "Project List"
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.lblStatus})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 239)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(315, 22)
+        Me.StatusStrip1.TabIndex = 5
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'lblStatus
+        '
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(0, 17)
         '
         'NPV_Calculator
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(315, 261)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.txtDiscountRate)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblCF)
         Me.Controls.Add(Me.btnCalc)
         Me.Controls.Add(Me.txtCfQuantity)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "NPV_Calculator"
         Me.Text = "NPV_Calculator"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -58,4 +131,11 @@ Partial Class NPV_Calculator
 
     Friend WithEvents txtCfQuantity As TextBox
     Friend WithEvents btnCalc As Button
+    Friend WithEvents lblCF As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtDiscountRate As TextBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ProjectListToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents lblStatus As ToolStripStatusLabel
 End Class
--- Final Project/NPV Calculator.vb
+++ Final Project/NPV Calculator.vb
@@ -1,52 +1,77 @@
 ﻿Option Explicit On
 Option Strict On
 
-'Test2
+
 
 Public Class NPV_Calculator
 
     Dim amount As Integer
+    Dim discountRate As Decimal
 
-    'Public Shared Function NPV(Rate As Double, ByRef ValueArray As Double()) As Double
-    'End Function
-
-    '' Define money format.
-    'Dim MoneyFmt As String = "###,##0.00"
-    '' Define percentage format.
-    'Dim PercentFmt As String = "#0.00"
-
-
-    '    ' Use the NPV function to calculate the net present value.
-    '    ' Set fixed internal rate.
-    '    Dim FixedRetRate As Double = 0.0625
-    '    ' Calculate net present value.
-    '    Dim NetPVal As Double = NPV(FixedRetRate, values)
-    '' Display net present value.
-    'MsgBox("The net present value of these cash flows is " & 
-    '    Format(NetPVal, MoneyFmt) & ".")
-
+    Public Sub Clear()
+        lblStatus.Text = ""
+        txtCfQuantity.Text = "0"
+        txtDiscountRate.Text = "."
+    End Sub
 
 
     Private Sub NPV_Calculator_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        txtCfQuantity.Text = CType(0, String)
+        Clear()
     End Sub
 
     Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
         Dim EnteredCF As Double 'The Cashflow entered by the user 
-        Dim CfAccumulator As Double = 0 'Accumulates the Cashflows after they are calculated
+        Dim CfAccumulator As Double = 0.00 'Accumulates the Cashflows after they are calculated
         Dim expCounter As Integer = 1   'Exponent counter, each period the denominator is raised by +1
 
+
+        'If txtDiscountRate.Text = "." Then 'Or CDec(txtDiscountRate.Text) < 0 Or CDec(txtDiscountRate.Text) > 1 Then
+        '    MessageBox.Show("Discount Rate is Invalid, insert rate between 0 and 1")
+        '    Me.Close()
+        'End If
+
+        'If discountRate < 0 Or discountRate > 1 Then
+        '    MessageBox.Show("Discount Rate is Invalid, insert rate between 0 and 1")
+        '    Me.Close()
+        'End If
+
+
+
+        If (Not Decimal.TryParse(txtDiscountRate.Text, discountRate)) Then             'Validates Price input as a decimal
+            lblStatus.Text = "Discount Rate is Invalid, insert rate between 0 and 1"
+            txtDiscountRate.Focus()
+            Return
+        End If
+
+
+        'Try
+        '    discountRate = CDec(txtDiscountRate.Text)
+        '    If txtDiscountRate.Text = "." Then
+        '        lblStatus.Text = "Discount Rate is Invalid, insert rate between 0 and 1"
+        '        txtDiscountRate.Focus()
+        '    ElseIf discountRate < 0 Or discountRate > 1 Then
+        '        lblStatus.Text = "Discount Rate is Invalid, insert rate between 0 and 1"
+        '        txtDiscountRate.Focus()
+        '    End If
+        '    Return
+        'Catch ex As Exception
+        '    lblStatus.Text = "Discount Rate is Invalid, insert rate between 0 and 1"
+        'End Try
+
         amount = CInt(txtCfQuantity.Text)           'assigns the user input to amount (amount of cashflow periods)
+
+
         Dim Values(amount - 1) As Double            'amount variable input defines the array length, - 1 since vba counts 0
 
         For Each amount As Decimal In Values 'displays input message boxes = the quantity of cash flow periods
 
             EnteredCF = CDbl(InputBox("Enter The Cashflow for this period", "Cash Flow", "0"))
 
-            CfAccumulator = CfAccumulator + CDbl(EnteredCF / (1 + 0.1) ^ expCounter) 'need to add array loop here to calculate denominator 
+            CfAccumulator = CfAccumulator + CDbl(EnteredCF / (1 + discountRate) ^ expCounter) 'need to add array loop here to calculate denominator 
 
             expCounter = expCounter + 1 'Increase exponent by 1 for each period
         Next
-        MessageBox.Show(CfAccumulator.ToString)         'For test only 
+
+        lblStatus.Text = "The Net Present Value of this Project Is" & CfAccumulator
     End Sub
 End Class
